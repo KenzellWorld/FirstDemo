ebbac46325e5109a09025748d46dbc7d3d2641cf
Steve <steve@Steves-MacBook-Pro.local>
2014-02-11 19:58:19 -0800

    Initial Commit

---
+++ FirstDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FirstDemo
+//
+//  Created by Steve on 2/11/14.
+//  Copyright (c) 2014 Steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FirstDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FirstDemo
+//
+//  Created by Steve on 2/11/14.
+//  Copyright (c) 2014 Steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
